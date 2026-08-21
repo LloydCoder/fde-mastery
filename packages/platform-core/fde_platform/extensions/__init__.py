bd5dbb8dc024856db47a721212ddb648fe32ee62
lloydcoder <140634407+LloydCoder@users.noreply.github.com>
2026-08-21 15:44:27 +0100

    feat: add extension marketplace contracts

---
+++ packages/platform-core/fde_platform/extensions/__init__.py
@@ -0,0 +1,23 @@
+"""Secure developer-extension and marketplace contracts."""
+
+from .marketplace import (
+    ArtifactProvenance,
+    Capability,
+    ExtensionManifest,
+    ExtensionRegistry,
+    ExtensionState,
+    PromotionDecision,
+    PromotionGate,
+    SignatureEnvelope,
+)
+
+__all__ = [
+    "ArtifactProvenance",
+    "Capability",
+    "ExtensionManifest",
+    "ExtensionRegistry",
+    "ExtensionState",
+    "PromotionDecision",
+    "PromotionGate",
+    "SignatureEnvelope",
+]
